20cdeb30e60c76b40adad2300f27710b462c69d4
ChetanPatil23 <chetan08rp@gmail.com>
2022-02-10 11:41:30 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
-import { About, Products, SingleProduct, PrivateRoute, Home, Error, Checkout, Cart } from './pages'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  About,
+  Products,
+  SingleProduct,
+  PrivateRoute,
+  Home,
+  Error,
+  Checkout,
+  Cart
+} from "./pages";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Sidebar/>
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route exact path='/about'>
-          <About/>
-        </Route>
-        <Route exact path='/products'>
-          <Products/>
-        </Route>
-        <Route exact path='/cart'>
-          <Cart/>
-        </Route>
-        <Route exact path='/checkout'>
-          <Checkout/>
-        </Route>
-        <Route exact path='/products/:id' children={<SingleProduct/>} />  
-        <Route exact path='*'>
-          <Error/>
-        </Route>
-      </Switch>
-    </Router>
-    <Footer/>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+          <Route exact path="/products/:id" children={<SingleProduct />} />
+          <Route exact path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
