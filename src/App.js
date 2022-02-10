e45267f6e47eb936ebc85dbf4d58aeb7f236abba
ChetanPatil23 <chetan08rp@gmail.com>
2022-02-10 10:41:53 +0530

    React-Router-Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import { About, Products, SingleProduct, PrivateRoute, Home, Error, Checkout, Cart } from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <>
+    <Navbar/>
+    <Sidebar/>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route exact path='/about'>
+          <About/>
+        </Route>
+        <Route exact path='/products'>
+          <Products/>
+        </Route>
+        <Route exact path='/cart'>
+          <Cart/>
+        </Route>
+        <Route exact path='/checkout'>
+          <Checkout/>
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct/>} />  
+        <Route exact path='*'>
+          <Error/>
+        </Route>
+      </Switch>
+    </Router>
+    <Footer/>
+    </>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import About from './AboutPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import PrivateRoute from './PrivateRoute'
+import Home from './HomePage'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import Cart from './CartPage'
+
+
+export { About, Products, SingleProduct, PrivateRoute, Home, Error, Checkout, Cart }
